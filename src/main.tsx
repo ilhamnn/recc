53dff376dfa27ca2c910bf90114f27d67fddf19b
madv . <ilhamnn17@gmail.com>
2026-02-18 13:39:45 +0700

    remove line footer

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./pages/App.tsx";
 import { Fot } from "./components/ui/footer.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
