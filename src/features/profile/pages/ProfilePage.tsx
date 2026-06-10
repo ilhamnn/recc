785250ca43799d9a62ec4677de599cf219e80e66
madv . <ilhamnn17@gmail.com>
2026-06-10 18:01:13 +0700

    fix bug on profile detail

--- src/features/profile/pages/ProfilePage.tsx
+++ src/features/profile/pages/ProfilePage.tsx
@@ -227,7 +227,7 @@ export default function ProfilePage() {
       alert("Hanya file gambar yang diizinkan");
       return;
     }
-
+    //asdasdasdasasdas
     setSheetOpen(false);
     setUploading(true);
 
